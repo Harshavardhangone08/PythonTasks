1baa391b6daa7f3e72e06c23d52d4676ff7347be
HarshaVardhan <harsh@HarshaVardhan.com>
2026-05-28 16:23:57 +0530

    GenAI sample project

---
+++ GenAI_App/sample/app.py
@@ -0,0 +1,38 @@
+# To run this code you need to install the following dependencies:
+# pip install google-genai
+
+import os
+from google import genai
+from google.genai import types
+from dotenv import load_dotenv
+
+
+load_dotenv()
+def generate(question:str):
+    client = genai.Client(
+        api_key=os.environ.get("GEMINI_API_KEY"),
+    )
+
+    model = "gemini-3-flash-preview"
+    contents = [
+        types.Content(
+            role="user",
+            parts=[
+                types.Part.from_text(text=question),
+            ],
+        ),
+    ]
+
+
+    for chunk in client.models.generate_content_stream(
+        model=model,
+        contents=contents,
+    ):
+        if text := chunk.text:
+            print(text, end="")
+
+if __name__ == "__main__":
+    question=input("Enter your question: ")
+    generate(question)
+
+
